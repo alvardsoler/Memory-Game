6f9599003c8d50d65be45fa7224d08594ddbfae3
alvardsoler <aigam666@gmail.com>
2016-02-24 15:19:49 +0100

    Cuerpo de las clases y cabecera de las funciones

--- src/practica1.js
+++ src/practica1.js
@@ -9,9 +9,39 @@ var MemoryGame = MemoryGame || {};
  * Constructora de MemoryGame
  */
 MemoryGame = function(gs) {
+    this.maps = gs.maps;
+    this.cards = new Array();
+    this.infoText = "MemoryGame";
+    this.numCardsFound = 0;
+    this.gs = gs;
 
+    console.log(this);
 };
 
+MemoryGame.prototype = {
+
+    // Inicializa el juego creando las cartas (2 de cada tipo), desordenándolas y comenzando
+    // el bucle del juego.
+    initGame: function() {
+        //alert("jugamos");
+    },
+    // Dibuja el juego
+    draw: function() {
+        // Escribir mensaje con el estado del juego
+
+        // Pedir a cada carta del tablero que se dibujen
+    },
+    // El bucle del juego.
+    loop: function() {
+        // Llamar al método draw cada 16 ms
+        setInterval(draw, 16);
+    },
+    // Se llama cada vez que se pulsa sobre una carta (identificada por la posición que ocupan
+    //	en el array). Voltea la carta, y si hay dos voleteadas, comprobar si son la misma.
+    onClick: function(cardId) {
+
+    }
+};
 
 
 /**
@@ -24,4 +54,21 @@ MemoryGame.Card = function(id) {
 
 };
 
+MemoryGame.Card.prototype = {
+    // Da la vuelta a la carta, cambiando su estado
+    flip: function() {
 
+    },
+    // Marca una carta como encontrada
+    found: function() {
+
+    },
+    // Compara dos cartas
+    compareTo: function(otherCard) {
+        return (this.id === otherCard.id);
+    },
+    // Dibuja la carta de acuerdo al estado en el que se encuentra la carta
+    draw: function(gs, pos) {
+
+    }
+};
