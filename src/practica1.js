e22dd5a24b024d86c24261fdd5e6b04cb016b30a
alvardsoler <aigam666@gmail.com>
2016-02-25 15:44:50 +0100

    Acabado en un principio

--- src/practica1.js
+++ src/practica1.js
@@ -10,25 +10,19 @@ var MemoryGame = MemoryGame || {};
  */
 MemoryGame = function(gs) {
     this.maps = gs.maps;
-    this.cards = new Array();
-    var i = 0;
+    this.cards = [];
+    this.flippedCards = [];
     for (var card in gs.maps) {
         if (card != "back") {
-            this.cards.push({
-                id: card,
-                pos: i
-            });
-            i++;
-            this.cards.push({
-                id: card,
-                pos: i
-            });
+            this.cards.push(new MemoryGame.Card(card));
+            this.cards.push(new MemoryGame.Card(card));
         }
     }
 
     this.infoText = "MemoryGame";
     this.numCardsFound = 0;
     this.gs = gs;
+    this.wait = false;
 
     //   console.log(this);
 };
@@ -40,25 +34,67 @@ MemoryGame.prototype = {
     initGame: function() {
         //alert("jugamos");        
         this.cards = shuffleArray(this.cards);
-        console.log(this);
+        //console.log(this);
+        this.loop();
     },
     // Dibuja el juego
     draw: function() {
         // Escribir mensaje con el estado del juego
-
+        this.gs.drawMessage(this.infoText);
         // Pedir a cada carta del tablero que se dibujen
+        for (var i = 0; i < this.cards.length; i++) {
+            this.cards[i].draw(this.gs, i);
+        }
     },
     // El bucle del juego.
     loop: function() {
+
         var self = this;
         // Llamar al método draw cada 16 ms
         setInterval(function() {
-            self.draw()
+            self.draw();
+            if (self.numCardsFound === (self.cards.length / 2)) {
+                self.infoText = "Win!";
+            }
         }, 16);
+
     },
     // Se llama cada vez que se pulsa sobre una carta (identificada por la posición que ocupan
     //	en el array). Voltea la carta, y si hay dos voleteadas, comprobar si son la misma.
     onClick: function(cardId) {
+        if (this.wait) return;
+        if (this.cards[cardId].state === "back") {
+            this.cards[cardId].flip();
+            this.flippedCards.push(this.cards[cardId]);
+        }
+        if (this.flippedCards.length == 2) {
+            if (this.flippedCards[0].compareTo(this.flippedCards[1])) {
+                // Las cartas son iguales, las marcamos como encontradas y aumentamos
+                // el número de cartas encontradas
+                this.flippedCards[0].found();
+                this.flippedCards[1].found();
+
+                this.numCardsFound++;
+                this.infoText = "Match!"
+                // Dejamos el array con tamaño 0
+                this.flippedCards = [];
+            } else {
+                // Son diferentes, las volteamos
+                var self = this;
+                this.wait = true;
+                this.infoText = "Fail!";
+                setTimeout(function() {
+                    self.flippedCards[0].flip();
+                    self.flippedCards[1].flip();
+                    // Dejamos el array con tamaño 0
+                    self.flippedCards = [];
+                    self.wait = false;
+                    self.infoText = "Try other!";
+                }, 500);
+
+            }
+
+        }
 
     }
 };
@@ -71,17 +107,42 @@ MemoryGame.prototype = {
  * @param {string} id Nombre del sprite que representa la carta
  */
 MemoryGame.Card = function(id) {
-
+    this.id = id;
+    this.state = "back";
+    this.found = false;
+    this.flip = function() {
+        if (this.state === "back") {
+            this.state = "face";
+        } else {
+            this.state = "back";
+        }
+    };
+    this.found = function() {
+        this.found = true;
+    };
+    this.compareTo = function(otherCard) {
+        return (this.id === otherCard.id);
+    };
+    this.draw = function(gs, pos) {
+        if (this.state === "back") {
+            gs.draw("back", pos);
+        } else
+            gs.draw(this.id, pos);
+    };
 };
-
+/*
 MemoryGame.Card.prototype = {
     // Da la vuelta a la carta, cambiando su estado
     flip: function() {
-
+        if (this.state === "back") {
+            this.state = "face";
+        } else {
+            this.state = "back";
+        }
     },
     // Marca una carta como encontrada
     found: function() {
-
+        this.found = true;
     },
     // Compara dos cartas
     compareTo: function(otherCard) {
@@ -89,9 +150,12 @@ MemoryGame.Card.prototype = {
     },
     // Dibuja la carta de acuerdo al estado en el que se encuentra la carta
     draw: function(gs, pos) {
-
+        if (this.state === "back") {
+            gs.draw("back", pos);
+        } else
+            gs.draw(this.id, pos);
     }
-};
+};*/
 
 /* Funciones auxiliares */
 
